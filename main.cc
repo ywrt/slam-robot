15ea7a72616159c76be65bbcfdfcfcfe344de08f
michael <michael@dgmo.org>
2012-12-24 20:03:02 +1100

    Add project geometry (aka homogenous points).

--- main.cc
+++ main.cc
@@ -14,6 +14,8 @@
 #include <vector>
 #include <map>
 
+#include <eigen3/Eigen/Eigen>
+
 using namespace std;
 using namespace cv;
 
@@ -37,9 +39,9 @@ struct SnavelyReprojectionError {
     ceres::QuaternionRotatePoint(camera_rotation, point, p);
 
     // camera[3,4,5] are the translation.
-    p[0] += camera_translation[0];
-    p[1] += camera_translation[1];
-    p[2] += camera_translation[2];
+    p[0] += camera_translation[0] * point[3];
+    p[1] += camera_translation[1] * point[3];
+    p[2] += camera_translation[2] * point[3];
     // Compute the center of distortion. The sign change comes from
     // the camera model that Noah Snavely’s Bundler assumes, whereby
     // the camera coordinate system has a negative z axis.
@@ -64,6 +66,7 @@ struct SnavelyReprojectionError {
   double observed_y;
 };
 
+
 struct Frame {
   Frame() :
     rotation_ {1,0,0,0},
@@ -71,6 +74,8 @@ struct Frame {
 
     double* rotation() { return &rotation_[0]; }
     double* translation() { return &translation_[0]; }
+    const double* rotation() const { return &rotation_[0]; }
+    const double* translation() const { return &translation_[0]; }
 
     double rotation_[4];
     double translation_[3];
@@ -79,15 +84,19 @@ struct Frame {
 };
 
 struct Camera {
-  Camera() : data {.683, -0.02, 0} {}
+  Camera() : data {1.3, -0.02, 0} {}
   double data[3];
   // 0 == focal length.
   // 1, 2 == radial distortion
+  double* scale() { return &data[0]; }
+  const double* scale() const { return &data[0]; }
+  double* instrinsics() { return &data[1]; }
+  const double* instrinsics() const { return &data[1]; }
 };
 
 struct RealPoint {
-  RealPoint() : data { 0, 0, 0 } {}
-  double data[3];
+  RealPoint() : data { 0, 0, 0, 1 } {}
+  double data[4];
 };
 
 struct Observation {
@@ -98,15 +107,18 @@ struct Observation {
   int prev_obs;
 };
 
-struct SortObs {
-  bool operator()(const Observation& a, const Observation& b) {
-    if (a.point_ref != b.point_ref)
-      return a.point_ref < b.point_ref;
-    if (a.frame_ref != b.frame_ref)
-      return a.frame_ref < b.frame_ref;
-    return a.pt.x < b.pt.x;
-  }
-};
+void Project(
+    const Camera& camera,
+    const Frame& frame,
+    const RealPoint& point,
+    double* result) {
+  SnavelyReprojectionError p(result[0],result[1]);
+  p(camera.scale(), camera.instrinsics(),
+      frame.rotation(),
+      frame.translation(),
+      &(point.data[0]),
+      result);
+}
 
 struct Descriptor {
   Descriptor(const uint8_t* v) {
@@ -156,17 +168,71 @@ struct Map {
   vector<DescribedPoint> descs;
 };
 
+class HomogenousParameterization : public ceres::LocalParameterization {
+public:
+ virtual ~HomogenousParameterization() {}
+ virtual bool Plus(const double* x,
+                   const double* delta,
+                   double* x_plus_delta) const;
+ virtual bool ComputeJacobian(const double* x,
+                              double* jacobian) const;
+ virtual int GlobalSize() const { return 4; }
+ virtual int LocalSize() const { return 3; }
+};
 
+bool HomogenousParameterization::Plus(const double* x,
+    const double* delta,
+    double* x_plus_delta) const {
+  double r[4];
+  r[0] = x[0] + delta[0];
+  r[1] = x[1] + delta[1];
+  r[2] = x[2] + delta[2];
+  r[3] = x[3];
+  double norm = sqrt(r[0]*r[0] + r[1]*r[1] + r[2]*r[2] + r[3]*r[3]);
+  x_plus_delta[0] = r[0] / norm;
+  x_plus_delta[1] = r[1] / norm;
+  x_plus_delta[2] = r[2] / norm;
+  x_plus_delta[3] = r[3] / norm;
+  return true;
+}
 
-void RunSlam(Map* map) {
+bool HomogenousParameterization::ComputeJacobian(
+    const double* x,
+    double* jacobian) const {
+
+  double ssq = x[0]*x[0] + x[1]*x[1] + x[2]*x[2] + x[3]*x[3];
+  double ssq32 = sqrt(ssq)*ssq;
+
+  jacobian[0] = 1/sqrt(ssq) - x[0]*x[0]/ssq32;
+  jacobian[3] = -x[0]*x[1]/ssq32;
+  jacobian[6] = -x[0]*x[2]/ssq32;
+  jacobian[9] = -x[0]*x[3]/ssq32;
+
+  jacobian[1] = -x[1]*x[0]/ssq32;
+  jacobian[4] = 1/sqrt(ssq) - x[1]*x[1]/ssq32;
+  jacobian[7] = -x[1]*x[2]/ssq32;
+  jacobian[10] = -x[1]*x[3]/ssq32;
+
+  jacobian[2] = -x[2]*x[0]/ssq32;
+  jacobian[5] = -x[2]*x[1]/ssq32;
+  jacobian[8] = 1/sqrt(ssq) - x[2]*x[2]/ssq32;
+  jacobian[11] = -x[2]*x[3]/ssq32;
+  return true;
+}
+
+void RunSlam(Map* map, int min_frame_to_solve) {
   // Create residuals for each observation in the bundle adjustment problem. The
   // parameters for cameras and points are added automatically.
   ceres::Problem problem;
   ceres::Solver::Options options;
 
-  set<Frame*> frame_set;
+  const int frame = map->frames.size() - 1;
 
-  auto loss = new ceres::CauchyLoss(.01);
+  set<Frame*> frame_set;
+  set<RealPoint*> point_set;
+
+  auto loss = new ceres::CauchyLoss(.005);
+  //auto loss = new ceres::HuberLoss(0.02);
 
   for (const auto& o : map->obs) {
     CHECK_GE(o.frame_ref, 0);
@@ -174,16 +240,12 @@ void RunSlam(Map* map) {
     CHECK_LT(o.frame_ref, map->frames.size());
     CHECK_LT(o.point_ref, map->points.size());
 
-    Point2f pt = o.pt;
-    pt.x = 1. - pt.x / 1024. * 2.;
-    pt.y = 1. - pt.y / 768. * 2.;
-
     // Each Residual block takes a point and a camera as input and outputs a 2
     // dimensional residual. Internally, the cost function stores the observed
     // image location and compares the reprojection against the observation.
     ceres::CostFunction* cost_function =
-        new ceres::AutoDiffCostFunction<SnavelyReprojectionError, 2, 1, 2, 4, 3, 3>(
-            new SnavelyReprojectionError(pt.x, pt.y));
+        new ceres::AutoDiffCostFunction<SnavelyReprojectionError, 2, 1, 2, 4, 3, 4>(
+            new SnavelyReprojectionError(o.pt.x, o.pt.y));
 
     problem.AddResidualBlock(cost_function,
         loss /* squared loss */ ,
@@ -194,12 +256,12 @@ void RunSlam(Map* map) {
         &(map->points[o.point_ref].data[0]));
     Frame* f = &(map->frames[o.frame_ref]);
     frame_set.insert(f);
+    RealPoint* p = &(map->points[o.point_ref]);
+    point_set.insert(p);
   }
 
   if (frame_set.size() < 2)
     return;
-
-  int frames = frame_set.size();
 
 #if 0
   options.use_inner_iterations = true;
@@ -233,33 +295,49 @@ void RunSlam(Map* map) {
     problem.SetParameterization(frame->rotation(),
         quaternion_parameterization);
   }
+  auto homogenous = new HomogenousParameterization;
+  for (auto pt : point_set) {
+    problem.SetParameterization(&(pt->data[0]), homogenous);
+  }
 
   problem.SetParameterBlockConstant(map->frames[0].translation());
   problem.SetParameterBlockConstant(map->frames[0].rotation());
-  //problem.SetParameterBlockConstant(&(map->camera.data[0]));
-  if (frames < 12)
+  if (frame < 100)
+  problem.SetParameterBlockConstant(&(map->camera.data[0]));
+  if (frame < 200)
     problem.SetParameterBlockConstant(&(map->camera.data[1]));
 
-  if ((frames%12) == 0) {
-    for (int i = 1; (i + 2) < (map->frames.size() / 2);  ++i) {
-      auto f = &(map->frames[i]);
-      if (!frame_set.count(f))
+  for (int i = 1; i < (int)map->frames.size() && i < min_frame_to_solve; ++i) {
+    auto f = &(map->frames[i]);
+    if (!frame_set.count(f))
+      continue;
+    problem.SetParameterBlockConstant(f->translation());
+    problem.SetParameterBlockConstant(f->rotation());
+  }
+
+  if (min_frame_to_solve >= 0) {
+    for (const auto& o : map->obs) {
+      auto& desc = map->descs[o.desc_ref];
+      if (desc.last_frame >= min_frame_to_solve)
         continue;
-      problem.SetParameterBlockConstant(f->translation());
+      problem.SetParameterBlockConstant(
+          &(map->points[o.point_ref].data[0]));
     }
   }
 
-  options.linear_solver_type = ceres::DENSE_SCHUR;
-  options.preconditioner_type = ceres::CLUSTER_TRIDIAGONAL;
+
+  options.linear_solver_type = ceres::ITERATIVE_SCHUR;
+  options.preconditioner_type = ceres::SCHUR_JACOBI;
   options.minimizer_progress_to_stdout = true;
   //options.use_inner_iterations = true;
   options.max_num_iterations = 5000;
-  options.function_tolerance = 1e-7;
-//  if (frames > 15) {
-//  options.use_nonmonotonic_steps = true;
-//  }
+  options.function_tolerance = 1e-8;
+  //  if (frames > 15) {
+  //  options.use_nonmonotonic_steps = true;
+  //  }
   options.use_block_amd = true;
-
+  options.num_threads = 3;
+  options.parameter_tolerance = 1e-9;
 
   ceres::Solver::Summary summary;
   ceres::Solve(options, &problem, &summary);
@@ -283,6 +361,14 @@ void UpdateMap(Map* map,
     motion[1] = f1.translation()[1] - f2.translation()[1];
     motion[2] = f1.translation()[2] - f2.translation()[2];
     Frame f = f1;
+    if (motion[0] < -1) motion[0] = -1;
+    if (motion[1] < -1) motion[1] = -1;
+    if (motion[2] < -1) motion[2] = -1;
+
+    if (motion[0] > 1) motion[0] = 1;
+    if (motion[1] > 1) motion[1] = 1;
+    if (motion[2] > 1) motion[2] = 1;
+
     f.translation()[0] += motion[0];
     f.translation()[1] += motion[1];
     f.translation()[2] += motion[2];
@@ -301,7 +387,7 @@ void UpdateMap(Map* map,
     int best_match = -1;
     for (int di = 0; di < map->descs.size(); ++di) {
       auto& d = map->descs[di];
-      if ((frame - d.last_frame) > 15)
+      if ((frame - d.last_frame) > 115)
         continue;
       int dist = d.desc.distance(ptr);
       if (dist < min_distance) {
@@ -309,7 +395,7 @@ void UpdateMap(Map* map,
         best_match = di;
       }
     }
-    printf("%3d: dist %d (%d)\n", i, min_distance, best_match);
+    //printf("%3d: dist %d (%d)\n", i, min_distance, best_match);
 
     if (min_distance < 12) {
       DescribedPoint* dp = &(map->descs[best_match]);
@@ -331,6 +417,8 @@ void UpdateMap(Map* map,
 
         Observation o;
         o.pt = dp->last_point;
+        o.pt.x = 1. - o.pt.x / 1024. * 2.;
+        o.pt.y = 1. - o.pt.y / 768. * 2.;
         o.frame_ref = dp->last_frame;
         o.point_ref = dp->point_ref;
         o.desc_ref = best_match;
@@ -340,13 +428,15 @@ void UpdateMap(Map* map,
       }
       Observation o;
       o.pt = key_points[i].pt;
+      o.pt.x = 1. - o.pt.x / 1024. * 2.;
+      o.pt.y = 1. - o.pt.y / 768. * 2.;
       o.frame_ref = frame;
       o.point_ref = dp->point_ref;
       o.desc_ref = best_match;
       o.prev_obs = dp->last_obs;
 
       dp->last_frame = frame;
-      dp->last_point = o.pt;
+      dp->last_point = key_points[i].pt;
       dp->last_obs = map->obs.size();
       ++dp->matches;
 
@@ -370,10 +460,8 @@ void NormMap(Map* map) {
     sum += f.translation()[2] * f.translation()[2];
     count++;
   }
-
   if (sum < 1)
     return;
-
   double scale = count / sum;
 #if 0
   for (auto& p : map->points) {
@@ -387,13 +475,36 @@ void NormMap(Map* map) {
     f.translation()[1] *= scale;
     f.translation()[2] *= scale;
   }
-
 }
-
 
 void DumpMap(Map* map) {
   // SortObs sorter;
   // sort(map->obs.begin(), map->obs.end(), sorter);
+  int err_hist[10] = {0,0,0,0,0,0,0,0,0,0};
+  for (auto& o : map->obs) {
+    double r[2];
+    r[0] = o.pt.x;
+    r[1] = o.pt.y;
+    Project(
+        map->camera,
+        map->frames[o.frame_ref],
+        map->points[o.point_ref],
+        r);
+    double err = sqrt(r[0]*r[0]+r[1]*r[1]) * 1000;
+    if (err < 10) {
+      ++err_hist[(int)err];
+      continue;
+    }
+    printf("frame %3d pt %3d : [%7.3f %7.3f] (%7.2f,%7.2f) -> %.2f\n",
+        o.frame_ref,
+        o.point_ref,
+        o.pt.x, o.pt.y,
+        r[0] * 1000, r[1] * 1000,
+        err);
+  }
+  for (size_t i = 0; i < 10; ++i) {
+    printf("err_hist: %2ld : %5d\n", i, err_hist[i]);
+  }
   for (auto& f : map->frames) {
     printf("(%f,%f,%f,%f) -> (%f, %f, %f)\n",
         f.rotation()[0], f.rotation()[1], f.rotation()[2], f.rotation()[3],
@@ -412,7 +523,6 @@ void DumpMap(Map* map) {
       map->camera.data[2]);
 }
 
-
 int main(int argc, char*argv[]) {
   if (argc < 2) {
     fprintf(stderr, "Usage: slam filename\n");
@@ -426,14 +536,16 @@ int main(int argc, char*argv[]) {
   cv::Mat out;
   cv::Mat grey;
 
-  Ptr<FeatureDetector> detector(new ORB());
+  Ptr<FeatureDetector> detector(new ORB(500, 1.2, 8));
   Ptr<DescriptorExtractor> extractor(DescriptorExtractor::create("FREAK"));
 
-  int frame = 0;
+  int frame = -1;
 
   Map map;
 
   while (vid.read(img)) {
+    frame++;
+
     cv::transpose(img, out);
     cv::cvtColor(out, grey, CV_RGB2GRAY);
 
@@ -456,11 +568,9 @@ int main(int argc, char*argv[]) {
       line(out, a - Point2f(2,-2), a + Point2f(2,-2), c, 1, 8);
     }
 
-
     for (const Observation& o : map.obs) {
       if (o.frame_ref != frame)
         continue;
-
       auto c = Scalar(0,0,255);
       const Point2f& a = o.pt;
       line(out, a - Point2f(5,5), a + Point2f(5,5), c, 2, 8);
@@ -472,24 +582,20 @@ int main(int argc, char*argv[]) {
         const Point2f& b = px->pt;
         line(out, a, b, Scalar(0,0,0), 1, 8);
       }
-
-    }
-
-    DumpMap(&map);
-
-    if (frame < 20) {
-      // map.camera = Camera();
-    }
-    if ((frame%3) == 0) {
-      //NormMap(&map);
-      RunSlam(&map);
-      DumpMap(&map);
-
     }
 
     cv::imshow( "Display window", out);
-    cv::waitKey(0);
-    frame++;
+
+    if ((frame%3) == 0) {
+   //   RunSlam(&map, frame - 15);
+      //DumpMap(&map);
+      RunSlam(&map, -1);
+      DumpMap(&map);
+      cv::waitKey(0);
+    }
+
+
+
   }
   return 0;
 }
