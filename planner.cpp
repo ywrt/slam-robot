f37f406e25a206b125505c09e5ea50df78dd3e74
michael <michael@dgmo.org>
2013-01-12 05:56:32 +1100

    Hacked in gradient descent motion planner.

--- planner.cpp
+++ planner.cpp
@@ -7,6 +7,12 @@
 
 #include "planner.h"
 
+#include <string>
+#include <iostream>
+
+#include <eigen3/Eigen/Eigen>
+
+
 #include <ompl/base/State.h>
 #include <ompl/control/SimpleSetup.h>
 #include <ompl/control/spaces/RealVectorControlSpace.h>
@@ -23,6 +29,7 @@ namespace ob = ompl::base;
 namespace og = ompl::geometric;
 namespace oc = ompl::control;
 
+using namespace std;
 
 bool isStateValid(const ob::State *state) {
   return true;
@@ -60,7 +67,7 @@ void SimpleCarODE(const oc::ODESolver::StateType& q,
   qdot[2] = velocity * tan(steeringAngle); // theta-dot
 }
 
-void planWithSimpleSetup(void) {
+void planOMPLWithSimpleSetup(void) {
   // construct the state space we are planning in
   ob::StateSpacePtr spacex(new ob::DubinsStateSpace());
   ob::RealVectorBounds bounds(2);
@@ -95,9 +102,9 @@ void planWithSimpleSetup(void) {
 
   ss.setStartAndGoalStates(start, goal, 0.05);
 
-  auto planner = new oc::KPIECE1(ss.getSpaceInformation());
-  planner->setNearestNeighbors<ompl::NearestNeighborsSqrtApprox>();
-  ss.setPlanner(ompl::base::PlannerPtr(planner));
+  //auto planner = new oc::KPIECE1(ss.getSpaceInformation());
+  //planner->setNearestNeighbors<ompl::NearestNeighborsSqrtApprox>();
+  //ss.setPlanner(ompl::base::PlannerPtr(planner));
 
   bool solved = ss.solve(10.0);
 
@@ -108,3 +115,203 @@ void planWithSimpleSetup(void) {
    ss.getSolutionPath().print(std::cout);
   }
 }
+
+std::string string_format(const std::string &fmt, ...) {
+    int size = 100;
+    std::string str;
+    va_list ap;
+    while (1) {
+        str.resize(size);
+        va_start(ap, fmt);
+        int n = vsnprintf((char *)str.c_str(), size, fmt.c_str(), ap);
+        va_end(ap);
+        if (n > -1 && n < size) {
+            str.resize(n);
+            return str;
+        }
+        if (n > -1)
+            size = n + 1;
+        else
+            size *= 2;
+    }
+    return str;
+}
+
+struct State {
+  Vector2d pos_;
+  double direction_;
+  double velocity_;
+  double steering_;
+  string ToString() const {
+    return string_format(
+        "[pos %5.3f %5.3f dir %5.3f vel %5.3f steer %5.3f]",
+        pos_(0), pos_(1),
+        direction_,
+        velocity_,
+        steering_);
+  }
+};
+
+double clamp(double v, double range) {
+  if (v < -range) v = -range;
+  if (v > range) v = range;
+  return v;
+}
+
+struct Control {
+  Control() : steering_(0), accel_(0) {}
+  Control(double steering, double accel) :
+    steering_(steering), accel_(accel) {}
+
+  void add(const Control& delta, double scale) {
+    steering_ = clamp(steering_ + scale * delta.steering_, M_PI*.9);
+    accel_ = clamp(accel_ + scale* delta.accel_, 1);
+  }
+  double norm() const {
+    return sqrt(steering_ * steering_ + accel_*accel_);
+  }
+
+  void normalize() {
+    double n = norm();
+    if (n == 0)
+      return;
+    steering_ /= n;
+    accel_ /= n;
+  }
+  double steering_;
+  double accel_;
+
+
+
+};
+
+
+
+
+State project(const State& current,
+              const Control& control,
+              double step) {
+  State result = current;
+  result.velocity_ += control.accel_ * step;
+  result.steering_ += control.steering_ * step;
+
+  result.steering_ = clamp(result.steering_, 1);
+
+  result.direction_ += tan(result.steering_) * result.velocity_;
+
+  while (result.direction_ < -M_PI) result.direction_ += M_PI * 2;
+  while (result.direction_ > +M_PI) result.direction_ -= M_PI * 2;
+
+  result.pos_(0) += cos(result.direction_) * result.velocity_;
+  result.pos_(1) += sin(result.direction_) * result.velocity_;
+
+  return result;
+}
+
+
+double distance(const State& s1, const State& s2) {
+  double sum(0);
+  sum += (s1.pos_ - s2.pos_).squaredNorm();
+
+#if 1
+  sum += (s1.velocity_ - s2.velocity_) * (s1.velocity_ - s2.velocity_);
+
+  double ddir = s1.direction_ - s2.direction_;
+  while (ddir < -M_PI) ddir += M_PI * 2;
+  while (ddir > +M_PI) ddir -= M_PI * 2;
+  sum += ddir * ddir;
+#endif
+
+  //cout << s1.ToString() << " v " << s2.ToString() << " = " << sum << endl;
+
+  return sum;
+}
+
+double min_distance(const State& current,
+                    const State& goal,
+                    const Control& control,
+                    double step) {
+  State s = current;
+  double dist = distance(current, goal);
+  for (int i = 0; i < 100; ++i) {
+    s = project(s, control, step);
+    double d = distance(s, goal) + (0.05 * i);
+    if (d < dist)
+      dist = d;
+  }
+  return dist;
+}
+
+double deriv(const State& curr,
+             const State& goal,
+             const Control& control,
+             const Control& delta) {
+
+  Control c = control;
+  c.add(delta, 1);
+  double dist = min_distance(curr, goal, control, 1e-2);
+  double min_dist = min_distance(curr, goal, c, 1e-2);
+
+  return dist - min_dist;
+}
+
+Control gradient(const State& curr,
+                 const State& goal,
+                 struct Control& control
+                 ) {
+
+  double h = 1e-3;
+  Control r(0,0);
+  double best = 0;
+  for (int i = -1 ; i < 2; ++i) {
+    for (int j = -1 ; j < 2; ++j) {
+      double d = deriv(curr, goal, control, Control(i*h, j*h));
+      if (d < best)
+        continue;
+      best = d;
+      if (d > 0) {
+        r.steering_ = i;
+        r.accel_ = j;
+      }
+    }
+  }
+  printf("%f, %f -> %f\n", r.steering_, r.accel_, best);
+
+  r.normalize();
+  return r;
+}
+
+
+void planWithSimpleSetup(void) {
+  State goal;
+  goal.pos_ << 5,0;
+  goal.velocity_ = 0;
+  goal.direction_ = 0;
+
+  State curr;
+  curr.pos_ << 0,0;
+  curr.velocity_ = 0;
+  curr.direction_ = 0;
+
+  Control c(0,0);
+  for (int i = 0; i < 25; ++i) {
+    Control delta = gradient(curr, goal, c);
+
+    c.add(delta, 0.1);
+
+    curr = project(curr, c, 0.1);
+    printf("%6.3f %6.3f @ vel %4.4f dir %5.2f (dir %6.3f accel %6.3f) [grad dir %f, accel %f]\n",
+           curr.pos_(0), curr.pos_(1),
+           curr.velocity_,
+           curr.direction_,
+           c.steering_,
+           c.accel_,
+           delta.steering_,
+           delta.accel_
+           );
+  }
+
+
+
+}
+
